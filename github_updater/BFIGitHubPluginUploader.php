739a84cfd5fa041466c000edafc6a87a0cbe5142
Louis <louis@interactivedimension.com>
2014-03-27 14:05:40 +0000

    Fixed Parsedown inclusion

--- github_updater/BFIGitHubPluginUploader.php
+++ github_updater/BFIGitHubPluginUploader.php
@@ -120,7 +120,7 @@ class BFIGitHubPluginUpdater {
         $response->download_link = $downloadLink;
 
         // We're going to parse the GitHub markdown release notes, include the parser
-        require_once( plugin_dir_path( __FILE__ ) . "github_updater/Parsedown.php" );
+        require_once( plugin_dir_path( __FILE__ ) . "Parsedown.php" );
 
         // Create tabs in the lightbox
         $response->sections = array(
